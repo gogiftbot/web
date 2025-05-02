012da76fd226defa6e5223d92f8db84779b1cc75
Denis Muraveiko <d.muraveiko@leenda.com>
2025-05-03 02:13:44 +0300

    init

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 import { withOptimize } from "@prisma/extension-optimize";
 
 const prisma = new PrismaClient()
-  .$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! }))
+  // .$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! }))
   .$extends(withAccelerate());
 
 export default prisma;
