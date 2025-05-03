88aaa7f89f79346b298127a8b5fbd024c132fc5e
Denis Muraveiko <d.muraveiko@leenda.com>
2025-05-03 14:53:09 +0300

    init

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -1,16 +1,6 @@
 import { getNfts } from "./actions";
 import PageWrapper from "./Wrapper";
 
-export async function getStaticProps() {
-  const res = await fetch("https://api.vercel.app/blog");
-  const data = await res.json();
-
-  return {
-    props: { data },
-    revalidate: 60,
-  };
-}
-
 export default async function Root() {
   const nfts = await getNfts();
   return <PageWrapper nfts={nfts} />;
