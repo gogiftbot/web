5f1b27539daf206763f36fa2f56b24d06fb75b8d
Denis Muraveiko <d.muraveiko@leenda.com>
2025-05-11 23:59:08 +0300

    init

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import { TransitionLink } from "@/components/PageTransition";
-import { CustomTextInput } from "@/components/TextInput";
-import { CopyButton } from "@/components/TextInput/CopyButton";
-import { Avatar, Box, Heading, Text, VStack } from "@chakra-ui/react";
-import { Dashboard } from "./components/Dashboard";
-import { Referral, ReferralProps } from "./components/Referral";
 import { AccountWithGifts } from "../api/account/selector";
 import { useEffect, useState } from "react";
 import Page from "./components/Page";
 
-export default function Root(props: { account: AccountWithGifts }) {
+export default function Root() {
   const [isLoading, setIsLoading] = useState(true);
   const [account, setAccount] = useState<AccountWithGifts | null>(null);
 
