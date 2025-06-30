ab6e0f0f5736f9f4e9ce320d898ba822ba278ded
Denis Muraveiko <d.muraveiko@leenda.com>
2025-06-30 17:56:45 +0300

    release-1.7.0

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  productionBrowserSourceMaps: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
+    serverSourceMaps: false,
     workerThreads: false,
     optimizePackageImports: ["@chakra-ui/react"],
     webpackMemoryOptimizations: true,
